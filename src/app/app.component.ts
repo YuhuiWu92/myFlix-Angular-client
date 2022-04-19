083c9b2ef24f85078dd8c263a40dfbdf83c9b5c2
YuhuiWu92 <94534171+YuhuiWu92@users.noreply.github.com>
2022-04-20 00:38:48 +0200

    add welcome page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {FetchApiDataService}from './fetch-api-data.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
+  data=[];
+  constructor(private movie:FetchApiDataService){
+    this.movie.getAllMovies().subscribe(data=>{
+      console.warn(data)
+      this.data=data
+    })
+  }
+  
 }
--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -15,6 +15,7 @@ export class FetchApiDataService {
 // Inject the HttpClient module to the constructor params
 // This will provide HttpClient to the entire class, making it available via this.http
   constructor(private http: HttpClient,private router: Router) {
+    this.http=http;
   }
  // Making the api call for the user registration endpoint
   public userRegistration(userDetails: any): Observable<any> {
