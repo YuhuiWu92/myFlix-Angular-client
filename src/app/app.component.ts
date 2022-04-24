f76b0ecb71a802dbb03b18a24556335297b6e56c
YuhuiWu92 <94534171+YuhuiWu92@users.noreply.github.com>
2022-04-25 00:33:50 +0200

    creat moviecard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component'; 
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 import { MatDialog } from '@angular/material/dialog';
 @Component({
   selector: 'app-root',
@@ -20,10 +21,16 @@ openUserRegistrationDialog(): void {
   }
 
 
-  openUserLoginDialog(): void {
+openUserLoginDialog(): void {
     this.dialog.open(UserLoginFormComponent, {
 // Assigning the dialog a width
     width: '280px'
+    });
+  }
+
+openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+    width: '500px'
     });
   }
 }
