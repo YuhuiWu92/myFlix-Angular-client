a25d4bf46e1e9dbd76756a4ab7f115697e15b26f
YuhuiWu92 <94534171+YuhuiWu92@users.noreply.github.com>
2022-04-24 20:54:20 +0200

    finish login page

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -32,7 +32,7 @@ registerUser(): void {
     .subscribe((result) => {
   // Logic for a successful user registration goes here! (To be implemented)
      this.dialogRef.close(); // This will close the modal on success!
-     this.snackBar.open('User registration successful', 'OK', {
+     this.snackBar.open(result, 'OK', {
         duration: 2000
      });
     }, (result) => {
